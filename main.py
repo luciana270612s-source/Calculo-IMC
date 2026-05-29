5a14502786d2b5c2df21d4b9943c740867b47dab
ucianasilva270612 <lucianasilva270612@gmail.com>
2026-05-29 12:24:47 +0100

    Update main.py
    Adiciona entrada de peso e altura

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
 print("Calculadora de IMC")
+
+peso = float(input("Introduza o seu peso em kg: "))
+altura = float(input("Introduza a sua altura em metros: "))
+
+print(f"Peso introduzido: {peso} kg")
+print(f"Altura introduzida: {altura} m")
